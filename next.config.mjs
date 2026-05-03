bf2502703c238a1445235470775e4fd8a3491c23
Hein Dauven <heindauven@gmail.com>
2026-05-03 18:28:06 +0200

    Deploy static site to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  trailingSlash: isGithubPages,
+  images: {
+    unoptimized: true
+  },
+  ...(isGithubPages
+    ? {
+        output: "export",
+        basePath: "/the-invisible-grid",
+        assetPrefix: "/the-invisible-grid/"
+      }
+    : {})
 };
 
 export default nextConfig;
